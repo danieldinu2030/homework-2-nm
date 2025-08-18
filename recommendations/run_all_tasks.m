66c9d59844d9ba3ca56037d3954c1e8a69b62205
Daniel-Ioan Dinu <daniel_dinu30@yahoo.com>
2025-08-18 12:12:52 +0300

    Fixed run_all_tasks.m

--- recommendations/run_all_tasks.m
+++ recommendations/run_all_tasks.m
@@ -1,13 +1,13 @@
 function run_all_tasks()
     % Parameters
-    path = 'data.csv';        % Path to the CSV file
+    path = 'data.csv';         % Path to the CSV file
     liked_theme = 13;          % Index of the liked theme
-    num_recoms = 5;           % Number of recommendations to generate
-    min_reviews = 2;         % Minimum number of reviews per theme
+    num_recoms = 5;            % Number of recommendations to generate
+    min_reviews = 2;           % Minimum number of reviews per theme
     num_features = 10;         % Number of SVD features to use
     
     % Generate the recommendations
-    recoms = recommendations(path, liked_theme, num_recoms, min_reviews, num_features);
+    recom_indices = recommendations(path, liked_theme, num_recoms, min_reviews, num_features);
 
     % Display recommendations with original Theme-IDs
     fprintf('Top %d recommended themes similar to Theme-%d:\n', num_recoms, liked_theme);
